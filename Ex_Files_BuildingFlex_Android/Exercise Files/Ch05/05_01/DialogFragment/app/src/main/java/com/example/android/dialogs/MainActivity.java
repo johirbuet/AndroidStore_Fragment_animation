3276f0b8029db6d8fedb139c2347e1851582e40f
Johirul Islam <johirbuet@gmail.com>
2017-07-04 18:58:30 -0500

    Android Local Data Storage, Fragment and Animation

---
+++ Ex_Files_BuildingFlex_Android/Exercise Files/Ch05/05_01/DialogFragment/app/src/main/java/com/example/android/dialogs/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.android.dialogs;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        if (fab == null) throw new AssertionError();
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog();
+            }
+        });
+    }
+
+    private void showDialog() {
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+        alertDialog.setTitle("Alert");
+        alertDialog.setMessage("Here's some important information!");
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
+    }
+
+}
---
+++ Ex_Files_BuildingFlex_Android/Exercise Files/Ch06/06_01/ViewPagerFragment/app/src/main/java/com/example/david/viewpager/ItemFragment.java
@@ -0,0 +1,57 @@
+package com.example.david.viewpager;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+public class ItemFragment extends Fragment {
+
+    private Product mProduct;
+
+    public static ItemFragment newInstance(Product product) {
+
+        Bundle args = new Bundle();
+
+        ItemFragment fragment = new ItemFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public ItemFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        ViewGroup rootView = (ViewGroup) inflater
+                .inflate(R.layout.fragment_detail, container, false);
+
+//      display text and image
+        TextView nameText = (TextView) rootView.findViewById(R.id.nameText);
+        nameText.setText(mProduct.getName());
+
+        TextView descriptionText = (TextView) rootView.findViewById(R.id.descriptionText);
+        descriptionText.setText(mProduct.getDescription());
+
+        NumberFormat formatter = NumberFormat.getCurrencyInstance();
+        String price = formatter.format(mProduct.getPrice());
+        TextView priceText = (TextView) rootView.findViewById(R.id.priceText);
+        priceText.setText(price);
+
+        String productId = mProduct.getProductId();
+        int imageResource = getActivity().getResources()
+                .getIdentifier(productId, "drawable", getActivity().getPackageName());
+        ImageView iv = (ImageView) rootView.findViewById(R.id.imageView);
+        iv.setImageResource(imageResource);
+
+        return rootView;
+    }
+
+}
